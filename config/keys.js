1b3af1b0137fe7d7ad3b6906b12b76daf18a117c
info-arnav <info@arnavgupta.net>
2020-12-08 15:20:47 +0530

    mongoose

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI:
-    "mongodb://localhost/arnavguptanetblogpost" ||
+    "mongodb://127.0.0.1:5000/myapparnavguptanetblogpost" ||
     "mongodb+srv://Arnav:Arnav300804@cluster0.ahuqv.mongodb.net/arnavguptanetblogpost?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
